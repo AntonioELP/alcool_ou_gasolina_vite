04233947b945446e3a25d3c4be409d9f23658dcf
AntonioELP <162220041+AntonioELP@users.noreply.github.com>
2025-03-23 15:52:12 -0300

    Enviando projeto para deploy

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import "./app.css"; // <- IMPORTA O CSS CORRETAMENTE
+import "./app.css"; 
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
